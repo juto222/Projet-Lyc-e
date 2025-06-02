814136527ef1d4e98d08eb5bcc99e7edd233c941
victor <138701355+arbredu35@users.noreply.github.com>
2025-06-02 14:46:42 +0200

    V2

--- main.py
+++ main.py
@@ -15,28 +15,28 @@ print("""
                        github.com/YourRepo/YourTool
 
 ═══════════════════════════════════════════════════════════════════════
-[1] 🔧 Tools
-    ├── [11] Malware Generator
-    ├── [12] Token Grabber
-    ├── [13] File Binder
-    └── [14] Reverse Shell Builder
+[1] 🔐 Mot de passe
+    ├── [1] Générateur de mot de passe
+    ├── [2] Vérificateur de mot de passe
+      
+[2] 🛡 Pentest
+    ├── [1] Virus
+    ├── [2] Outil DDoS
+    ├── [3] Générateur de fausse page HTML
 
-[2] 💰 Premium Tools
-    ├── [21] Python Crypter
-    ├── [22] Discord Injection
-    ├── [23] Cloudflare Bypass
-    └── [24] Obfuscation Engine
+[3] 📊 Réseau
+    ├── [1] Ping IP
+    ├── [2] Journal / Logs
 
-[3] 🎮 Platform Exploits
-    ├── [31] Roblox Cookie Dump
-    ├── [32] Roblox API Tools
-    └── [33] Roblox Webhook Spammer
-
-[4] 📜 About / Legal
-    ├── [41] Info
-    ├── [42] Disclaimer
-    └── [43] Exit
-
+[4] ⚙️ Paramètres
+    ├── [1] Mode sombre / clair
+    ├── [2] Choix de langue (FR/EN)
+    └── [3] Exit
+      
+[4] ⚖️ Aide & Légalité
+    ├── [1] Documentation utilisateur
+    ├── [2] FAQ
+    ├── [3] Mentions légales & avertissement d’usage
+    └── [4] Informations RGPD
 ═══════════════════════════════════════════════════════════════════════
-Select Option [#] ──> █
     """)
