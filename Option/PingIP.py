363cfc41a3db4f645c61a1ff0b290f00d6b48e98
juto222 <thomasjulien3101@gmail.com>
2025-06-11 15:39:52 +0200

    Add files via upload

---
+++ Option/PingIP.py
@@ -0,0 +1,20 @@
+import requests
+
+print("""
+
+
+
+
+
+""")
+
+ip = input("Entrez le domaine à scanner (ex : https://exemple.com): ")
+
+try:
+    scan = requests.get(ip, timeout=5)
+    if scan.status_code == 200:
+        print(f"L'IP est en ligne (code {scan.status_code})")
+    else:
+        print(f"L'IP a répondu avec le code {scan.status_code}")
+except Exception as e:
+    print(f"Une erreur est survenue le site est inaccessible ou hors ligne: {e}")
