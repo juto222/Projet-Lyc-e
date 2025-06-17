566d7d16c7cb0247641b8afa0f4ccf115e24b716
victor <138701355+arbredu35@users.noreply.github.com>
2025-06-17 12:19:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from Option import PingIP
+from Option import CheckMDP
+from Option import VerifMDP
+
+
 print("""                                                                                                                                                                                                                                                                                                                                                 
              @                                                                                                                  
           @@@@@@                                                                                                                
@@ -16,27 +21,33 @@ print("""
 
 ═══════════════════════════════════════════════════════════════════════
 [1] 🔐 Mot de passe
-    ├── [1] Générateur de mot de passe
-    ├── [2] Vérificateur de mot de passe
+    ├── [11] Générateur de mot de passe
+    ├── [12] Vérificateur de mot de passe
       
 [2] 🛡 Pentest
-    ├── [1] Virus
-    ├── [2] Outil DDoS
-    ├── [3] Générateur de fausse page HTML
+    ├── [21] Virus
+    ├── [22] Outil DDoS
+    ├── [23] Générateur de fausse page HTML
 
 [3] 📊 Réseau
-    ├── [1] Ping IP
-    ├── [2] Journal / Logs
+    ├── [31] Ping IP
+    ├── [32] Journal / Logs
 
 [4] ⚙️ Paramètres
-    ├── [1] Mode sombre / clair
-    ├── [2] Choix de langue (FR/EN)
-    └── [3] Exit
+    ├── [41] Mode sombre / clair
+    ├── [42] Choix de langue (FR/EN)
+    └── [43] Exit
       
-[4] ⚖️ Aide & Légalité
-    ├── [1] Documentation utilisateur
-    ├── [2] FAQ
-    ├── [3] Mentions légales & avertissement d’usage
-    └── [4] Informations RGPD
+[5] ⚖️ Aide & Légalité
+    ├── [51] Documentation utilisateur
+    ├── [52] FAQ
+    ├── [53] Mentions légales & avertissement d’usage
+    └── [54] Informations RGPD
 ═══════════════════════════════════════════════════════════════════════
     """)
+
+choix = int(input("Entrez l'option de votre choix : "))
+
+if choix == 11:
+  try:
+    
