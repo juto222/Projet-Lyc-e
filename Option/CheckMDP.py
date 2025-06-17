dc801e78b18fd4d0fda78cedfef928381dca0387
victor <138701355+arbredu35@users.noreply.github.com>
2025-06-17 12:26:57 +0200

    Update CheckMDP.py

--- Option/CheckMDP.py
+++ Option/CheckMDP.py
@@ -1,35 +1,35 @@
 import string
 
-print("""
+    print("""
 
 
 
 
 
-""")
+    """)
 
-mdp = input("Entrez le mot de passe à vérifier :")
+    mdp = input("Entrez le mot de passe à vérifier :")
 
-score = 0
+    score = 0
 
-if len(mdp) >= 8:
-    score += 1
-else:
-    print("Le mot de passe doit contenir plus de 8 caractères.")
+    if len(mdp) >= 8:
+        score += 1
+    else:
+        print("Le mot de passe doit contenir plus de 8 caractères.")
 
-if any(c in string.digits for c in mdp):
-    score += 1
+    if any(c in string.digits for c in mdp):
+        score += 1
 
-if any(c in string.punctuation for c in mdp):
-    score += 1
+    if any(c in string.punctuation for c in mdp):
+        score += 1
 
-if any(c in string.ascii_uppercase for c in mdp):
-    score += 1 
+    if any(c in string.ascii_uppercase for c in mdp):
+        score += 1 
 
-if score == 4:
-    print("Mot de passe très fort")
-elif 2 <= score < 4:
-    print("Mot de passe moyen")
-else:
-    print("Mot de passe faible")
+    if score == 4:
+        print("Mot de passe très fort")
+    elif 2 <= score < 4:
+        print("Mot de passe moyen")
+    else:
+        print("Mot de passe faible")
 
