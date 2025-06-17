dc039d9ef6456ab14ba2c3cc640cf25559d5070e
victor <138701355+arbredu35@users.noreply.github.com>
2025-06-17 12:20:44 +0200

    Update PingIP.py

--- Option/PingIP.py
+++ Option/PingIP.py
@@ -1,20 +1,23 @@
 import requests
 
-print("""
+
+def ping():
+    
+    print("""
 
 
 
 
 
-""")
+    """)
 
-ip = input("Entrez le domaine à scanner (ex : https://exemple.com): ")
+    ip = input("Entrez le domaine à scanner (ex : https://exemple.com): ")
 
-try:
-    scan = requests.get(ip, timeout=5)
-    if scan.status_code == 200:
-        print(f"L'IP est en ligne (code {scan.status_code})")
-    else:
-        print(f"L'IP a répondu avec le code {scan.status_code}")
-except Exception as e:
-    print(f"Une erreur est survenue le site est inaccessible ou hors ligne: {e}")
+    try:
+        scan = requests.get(ip, timeout=5)
+        if scan.status_code == 200:
+            print(f"L'IP est en ligne (code {scan.status_code})")
+        else:
+            print(f"L'IP a répondu avec le code {scan.status_code}")
+    except Exception as e:
+        print(f"Une erreur est survenue le site est inaccessible ou hors ligne: {e}")
