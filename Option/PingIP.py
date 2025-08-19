b0767c962f14f856584a34da252b71ebb4f7e693
juto222 <thomasjulien3101@gmail.com>
2025-08-19 16:39:44 +0200

    Update PingIP.py

--- Option/PingIP.py
+++ Option/PingIP.py
@@ -1,4 +1,6 @@
 import requests
+import time
+
 
 
 def ping():
@@ -13,11 +15,31 @@ def ping():
 
     ip = input("Entrez le domaine à scanner (ex : https://exemple.com): ")
 
+    # Scan
     try:
         scan = requests.get(ip, timeout=5)
         if scan.status_code == 200:
-            print(f"L'IP est en ligne (code {scan.status_code})")
+            status = "En ligne !"
+            print(f"L'IP est en ligne (code {scan.status_code})\n")
+            print("Les logs sont disponible dans le fichier logs.txt\n")
         else:
-            print(f"L'IP a répondu avec le code {scan.status_code}")
+            status = "Hors ligne !"
+            print(f"L'IP a répondu avec le code {scan.status_code} \n")
+            print("Les logs sont disponible dans le fichier logs.txt \n")
     except Exception as e:
-        print(f"Une erreur est survenue le site est inaccessible ou hors ligne: {e}")
+        status = f"Hors ligne (erreur : {e})"
+        print(f"Une erreur est survenue le site est inaccessible ou hors ligne: {e}\n")
+        print("Les logs sont disponible dans le fichier logs.txt\n")
+
+    # Affiche les logs
+    with open("logs.txt", "a") as fichier:
+        fichier.write(
+            f"------------------------------------\n"
+            f"\n"
+            f"[{time.strftime('%d-%m-%Y %H:%M:%S')}] Ping {ip} : {status}\n"
+            f"\n"
+            f"------------------------------------\n"
+            f"\n"
+        )
+
+ping()
