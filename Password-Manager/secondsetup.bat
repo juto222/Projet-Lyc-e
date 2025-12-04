3c508e5eb714e0bce5463262daef50ebd224e735
juto222 <thomasjulien3101@gmail.com>
2025-12-04 11:34:24 +0100

    Update secondsetup.bat

--- Password-Manager/secondsetup.bat
+++ Password-Manager/secondsetup.bat
@@ -10,14 +10,14 @@ python -m pip install --upgrade pip
 REM Installer les librairies nécessaires
 echo Installation des librairies...
 
-pip install flet
-pip install hashlib
-pip install cryptography pyperclip
-pip install requests
-pip install time
-pip install bs4
-pip install aiohttp
-pip install asyncio
+python3 -m pip install flet
+python3 -m pip install hashlib
+python3 -m pip install cryptography pyperclip
+python3 -m pip install requests
+python3 -m pip install time
+python3 -m pip install bs4
+python3 -m pip install aiohttp
+python3 -m pip install asyncio
 
 echo Toutes les librairies ont été installées.
 pause
